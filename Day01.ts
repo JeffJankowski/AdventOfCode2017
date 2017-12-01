cc6d24602fd4b96e2db75a0ba42823a142214bf2
Jeff Jankowski <jeffrey.jankowski@gmail.com>
2017-11-30 22:59:59 -0800

    Adding linting rules

--- Day01.ts
+++ Day01.ts
@@ -1,23 +1,24 @@
-import fs = require('fs')
+import fs = require('fs');
 
 function captcha(data: string) {
     return (next: (n: number) => number) => {
         let sum = 0;
         for (let i = 0; i < data.length; i++) {
-            if (data[i] === data[next(i)])
-                sum += parseInt(data[i]);
+            if (data[i] === data[next(i)]) {
+                sum += parseInt(data[i], 10);
+            }
         }
         return sum;
-    }
+    };
 }
 
-let data = fs.readFileSync('data/day01.txt', 'utf8');
+const input = fs.readFileSync('data/day01.txt', 'utf8');
 // trying out some partial application-style composition
-let withData = captcha(data);
+const withData = captcha(input);
 
-//part 1
-let seq = (i: number) => (i + 1) % data.length;
+// part 1
+const seq = (i: number) => (i + 1) % input.length;
 console.log(`Using sequential digit: ${withData(seq)}`);
-//part 2
-let halfway = (i: number) => (i + data.length / 2) % data.length;
+// part 2
+const halfway = (i: number) => (i + input.length / 2) % input.length;
 console.log(`Using halfway digit:    ${withData(halfway)}`);
