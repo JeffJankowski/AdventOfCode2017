2bf96bde354fa99ce523842925a8c498022a447f
Jeff Jankowski <jeffrey.jankowski@gmail.com>
2017-11-30 22:24:19 -0800

    first day completed! CAPTCHA digits

--- Day01.ts
+++ Day01.ts
@@ -1,11 +1,23 @@
 import fs = require('fs')
-import comb = require('js-combinatorics')
 
-class Day01 {
-    public static main() {
-        const data = fs.readFileSync('data/day01.txt', 'utf8').split(/\s/);
-        console.log(comb.factorial(4));
+function captcha(data: string) {
+    return (next: (n: number) => number) => {
+        let sum = 0;
+        for (let i = 0; i < data.length; i++) {
+            if (data[i] === data[next(i)])
+                sum += parseInt(data[i]);
+        }
+        return sum;
     }
 }
 
-Day01.main();
+let data = fs.readFileSync('data/day01.txt', 'utf8');
+// trying out some partial application-style composition
+let withData = captcha(data);
+
+//part 1
+let seq = (i: number) => (i + 1) % data.length;
+console.log(`Using sequential digit: ${withData(seq)}`);
+//part 2
+let halfway = (i: number) => (i + data.length / 2) % data.length
+console.log(`Using halfway digit:    ${withData(halfway)}`);
