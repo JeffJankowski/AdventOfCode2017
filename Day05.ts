da8faf2ab24c757c8afea2752e6129681e74a93c
Jeff Jankowski <jeffrey.jankowski@gmail.com>
2017-12-05 15:03:18 -0800

    I forgot about default parameter values

--- Day05.ts
+++ Day05.ts
@@ -1,6 +1,6 @@
 import fs = require('fs');
 
-function steps(instructions: number[], incrFunc: (off: number) => number) {
+function steps(instructions: number[], incrFunc = (n: number) => 1) {
     const data = [...instructions];
     let pc = 0;
     let count = 0;
@@ -14,6 +14,6 @@ function steps(instructions: number[], incrFunc: (off: number) => number) {
     return count;
 }
 
-const input = fs.readFileSync('data/day05.txt', 'utf8').split('\n').map((num) => parseInt(num, 10));
-console.log(`Number of strange jumps:  ${steps(input, (_) => 1)}`);
-console.log(`Number of stranger jumps: ${steps(input, (jmp) => jmp >= 3 ? -1 : 1)}`);
+const input = fs.readFileSync('data/day05.txt', 'utf8').split('\n').map((n) => parseInt(n, 10));
+console.log(`Number of strange jumps:  ${steps(input)}`);
+console.log(`Number of stranger jumps: ${steps(input, (n) => n >= 3 ? -1 : 1)}`);
