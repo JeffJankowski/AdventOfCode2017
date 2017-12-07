4a2e0c33dac827dbe253264c40f8df3503a8ef2c
Jeff Jankowski <jeffrey.jankowski@gmail.com>
2017-12-07 13:05:26 -0800

    Tiny bit of cleanup, leaving it nasty

--- Day07.ts
+++ Day07.ts
@@ -18,48 +18,41 @@ function build(data: string[]): [Map<string, number>, Map<string, string[]>] {
 
 function calcTotals(weights: Map<string, number>, adjs: Map<string, string[]>, root: string) {
     const totals = new Map<string, number>();
-
     function calc(id: string): number {
         if (totals.has(id)) {
             return totals.get(id) as number;
         }
-
         const total = (weights.get(id) as number) +
             (adjs.get(id) || []).reduce((sum, above, i, a) => sum += calc(above), 0);
         totals.set(id, total);
         return total;
     }
-
-    calc(bottom);
+    const _ = calc(bottom);
     return totals;
 }
 
 function unbalanced(totals: Map<string, number>, adjs: Map<string, string[]>, root: string) {
     function find(id: string, balance: number): [string, number] {
-        const weightCount = new Map<number, string[]>();
+        const invTotals = new Map<number, string[]>();
         (adjs.get(id) || []).forEach((above) => {
             const weight = totals.get(above) as number;
-            weightCount.set(weight, (weightCount.get(weight) || []).concat(above));
+            invTotals.set(weight, (invTotals.get(weight) || []).concat(above));
         });
-
-        if (weightCount.size === 1) {
+        if (invTotals.size === 1) {
             return [id, balance];
         }
-
-        const sorted = [...weightCount.entries()]
-            .sort(([wA, idsA], [wB, idsB]) => idsA.length - idsB.length);
+        const sorted = [...invTotals.entries()]
+            .sort(([_, idsA], [__, idsB]) => idsA.length - idsB.length);
         return find(sorted[0][1][0], sorted[1][0] - sorted[0][0]);
     }
-
     return find(root, 0);
 }
 
 const [weightMap, adjMap] = build(fs.readFileSync("data/day07.txt", "utf8").split("\r\n"));
 const allAbove = ([] as string[]).concat(...(adjMap.values() as IterableIterator<string[]>));
 const bottom = [...adjMap.keys()].find((id) => allAbove.indexOf(id) === -1) || "";
-
-console.log(bottom);
+console.log(`Program on bottom: ${bottom}`);
 
 const totalsMap = calcTotals(weightMap, adjMap, bottom);
 const [node, offset] = unbalanced(totalsMap, adjMap, bottom);
-console.log(node, (weightMap.get(node) as number) + offset);
+console.log(`Unbalanced program (${node}) needs weight: ${(weightMap.get(node) as number) + offset}`);
